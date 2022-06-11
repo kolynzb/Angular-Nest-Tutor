0d4643a0f151190d75c4db24943a8e9d32000b2d
kolynzb <collinsbenda360@gmail.com>
2022-06-11 07:04:16 +0300

    Create user entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeORMConfigAsync } from './config/typeorm.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfigAsync), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/tut.md
+++ server/tut.md
@@ -1,2 +1,8 @@
 https://docs.nestjs.com/techniques/database
 `yarn add @nestjs/typeorm typeorm mysql2`
+
+adding env variables
+https://youtu.be/aDlBnxVzS_Q
+
+generate modules
+`nest g module <module_name>`
