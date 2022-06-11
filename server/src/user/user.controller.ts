3c65c159f28281b2dd3ce140a378e0a8beaf32cd
kolynzb <collinsbenda360@gmail.com>
2022-06-11 08:17:13 +0300

    Register User

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { UserService } from './user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller('api')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Post('register')
-  register(@Body() body: any) {
-    return body;
+  async register(@Body() body: any) {
+    if (body.password !== body.password_confirmation) {
+      throw new BadRequestException(
+        'Password and password confirmation do not match',
+      );
+    }
+
+    return this.userService.save({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: await bcrypt.hash(body.password, 12),
+    });
   }
 }
